cf4c10b7a4f0f749c72cc3987e6aee10b0376687
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-22 01:13:46 +0500

    add first example

--- intro_inheritance.py
+++ intro_inheritance.py
@@ -1,1 +1,22 @@
-print("hello world")
+class Phone:
+    def __init__(self, make, model, _price):
+        self.make = make
+        self.model = model
+        self._price = _price
+
+    def make_call(self, number):
+        print(f"Calling {number}")
+
+    def full_name(self):
+        return f"{self.make} {self.model} and price is {self._price}"
+
+
+class SmartPhone(Phone):
+    def __init__(self, make, model, _price, ram, memory, camera):
+        super.__init__(make, model, _price)
+        self.make = make
+        self.model = model
+        self._price = _price
+        self.ram = ram
+        self.memory = memory
+        self.camera = camera
