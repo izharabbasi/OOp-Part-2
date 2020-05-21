44cf9a463479f4e3fe63140372c99c08950144b2
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-22 01:20:53 +0500

    add final example

--- intro_inheritance.py
+++ intro_inheritance.py
@@ -13,10 +13,15 @@ class Phone:
 
 class SmartPhone(Phone):
     def __init__(self, make, model, _price, ram, memory, camera):
-        super.__init__(make, model, _price)
-        self.make = make
-        self.model = model
-        self._price = _price
+        super().__init__(make, model, _price)
         self.ram = ram
         self.memory = memory
         self.camera = camera
+
+
+phone = Phone("Nokia ", "1100", 1000)
+smartPhone = SmartPhone("iphone", "7plus", 6500, "4GB", "128GB", "12MP")
+
+print(smartPhone.make)
+print(smartPhone._price)
+print(smartPhone.full_name())
