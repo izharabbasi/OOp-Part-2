3c7e542daf0ab00b4211c3de9e84e6653227b048
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-22 23:45:08 +0500

    add second state

--- raise_error.py
+++ raise_error.py
@@ -1,1 +1,7 @@
-print("hello world")
+def add(a, b):
+    if(type(a) == int and type(b) == int):
+        return a+b
+    raise TypeError("Opps there is a issue with your value")
+
+
+print(add(2, 3))
