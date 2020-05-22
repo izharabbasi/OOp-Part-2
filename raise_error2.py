0637b4ec0cca64e9555e95df168cd00c87679439
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-23 00:15:15 +0500

    add 2nd state

--- raise_error2.py
+++ raise_error2.py
@@ -1,1 +1,15 @@
-print("hello world")
+class Mobile:
+    def __init__(self, name):
+        self.name = name
+
+
+class MobStore:
+    def __init__(self):
+        self.mobile = []
+
+    def add_phone(self, new_mobile):
+        return self.mobile.append(new_mobile)
+
+
+phone = Mobile("one plus")
+print(phone.name)
