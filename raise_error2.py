89b5a50ece0c0afec8bb06596cfb5942380b7146
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-23 00:22:22 +0500

    add final examplr

--- raise_error2.py
+++ raise_error2.py
@@ -8,8 +8,15 @@ class MobStore:
         self.mobile = []
 
     def add_phone(self, new_mobile):
-        return self.mobile.append(new_mobile)
+        if isinstance(new_mobile, Mobile):
+            self.mobile.append(new_mobile)
+        else:
+            raise TypeError("you cannot pass ")
 
 
 phone = Mobile("one plus")
-print(phone.name)
+samsung = "s8"
+
+mobile_store = MobStore()
+mobile_store.add_phone(phone)
+print(mobile_store.mobile)
